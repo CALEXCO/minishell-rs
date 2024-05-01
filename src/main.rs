4e719cc4c74d9642ee35ce7d9a6c8b713daeedbe
CALEXCO <82407596+CALEXCO@users.noreply.github.com>
2024-05-01 10:38:19 +0200

    Feature/parser (#4)
    * Create an instance of parser
    
    * Adding Workflows
    
    * Renaming
    
    * First version of parser and tests
    
    * Refactor file
    
    ---------
    
    Co-authored-by: Pmarquez <48651252+pxp9@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,51 @@
+use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "./parser_files/code.pest"] // relative to src
-struct MyParser;
+pub struct MyParser;
 
 fn main() {
-    let _parser = MyParser;
+    let tokens =
+        MyParser::parse(Rule::command_line, "cd -a|ls|grep").unwrap_or_else(|e| panic!("{}", e));
+    for token in tokens {
+        println!("{:?}", token.as_rule());
+        println!("{:?}", token.as_span());
+        println!("{}", token.as_str())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_command() {
+        let input = "ls -la | grep test";
+        assert!(MyParser::parse(Rule::command_line, input).is_ok());
+    }
+
+    #[test]
+    fn test_command_with_logical_operator() {
+        let input = "cd arg && ls";
+        assert!(MyParser::parse(Rule::command_line, input).is_err());
+    }
+
+    #[test]
+    fn test_command_with_redirection() {
+        let input = "echo hello > output.txt";
+        assert!(MyParser::parse(Rule::command_line, input).is_err());
+    }
+
+    #[test]
+    fn test_command_with_options_and_arguments() {
+        let input = "rm -rf folder_name";
+        assert!(MyParser::parse(Rule::command_single_line, input).is_ok());
+    }
+
+    #[test]
+    fn test_invalid_syntax() {
+        let input = "cp sourcefile||destinationfile";
+        assert!(MyParser::parse(Rule::command_line, input).is_err());
+    }
 }
